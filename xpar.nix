0f3425c6db623b0c3df86ea9ddc5d0be1a78169d
alexpkgs <alexzsk@outlook.com>
2024-10-04 23:09:09 -0400

    Update xpar.nix

--- xpar.nix
+++ xpar.nix
@@ -4,29 +4,29 @@ let
     background2 = "#131111";
     background3 = "#131111";
     text = "#bbb6b6";
-    caret = "#E8E3E3";
-    accent = "#E8E3E3";
-    dim = "#424242";
-    divider = "#151515";
-    selection = "#2e2e2e";
-    lineNumber = "#424242";
-    lineNumber2 = "#E8E3E3";
-    lineHighlight = "#151515";
-    scrollbar = "#121212";
-    scrollbar2 = "#101010";
+    caret = "#E8E3E3"; # usually for lite-xl
+    accent = "#E8E3E3"; # usually for lite-xl
+    dim = "#424242"; # usually for lite-xl
+    divider = "#151515"; # usually for lite-xl
+    selection = "#2e2e2e"; # usually for lite-xl
+    lineNumber = "#424242"; # usually for lite-xl
+    lineNumber2 = "#E8E3E3"; # usually for lite-xl
+    lineHighlight = "#151515"; # usually for lite-xl
+    scrollbar = "#121212"; # usually for lite-xl
+    scrollbar2 = "#101010"; # usually for lite-xl
   };
 
   syntaxColors = {
-    normal = "#E8E3E3";
-    symbol = "#b66467";
-    comment = "#424242";
-    keyword = "#A988B0";
-    keyword2 = "#A988B0";
-    number = "#D9BC8C";
-    literal = "#E8E3E3";
-    string = "#8C977D";
-    operator = "#A988B0";
-    function = "#8DA3B9";
+    normal = "#E8E3E3"; # usually for lite-xl
+    symbol = "#b66467"; # usually for lite-xl
+    comment = "#424242"; # usually for lite-xl
+    keyword = "#A988B0"; # usually for lite-xl
+    keyword2 = "#A988B0"; # usually for lite-xl
+    number = "#D9BC8C"; # usually for lite-xl
+    literal = "#E8E3E3"; # usually for lite-xl
+    string = "#8C977D"; # usually for lite-xl
+    operator = "#A988B0"; # usually for lite-xl
+    function = "#8DA3B9"; # usually for lite-xl
   };
 in
 {
